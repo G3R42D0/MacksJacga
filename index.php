a0ac9c48c2286125eab76e9cb34369362c7fd5ff
Gera <g420.bg@gmail.com>
2018-10-23 12:40:31 -0500

    Subida
    Uno

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+?>
+<html>
+	<head>
+		<title>LogIn CECYTE 10</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include "php/navbar.php"; ?>
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12">
+			<h2>REGISTRO Y LOGIN BASICO</h2>
+			<p class="lead">Sistema de Registro y Login Sencillo UNIVERSIDAD DEL VALLE DE TLAXCALA</p>
+			<p>Cecyte es tu mejor opcion <b>rl-php</b> Cuarto E</p>
+			<p>Instrucciones:</p>
+			<ol>
+				<li>Registrate en la opcion de registro.</li>
+				<li>Inicie sesion en la opcion de login.</li>
+				<li>Para finalizar la sesion, click en la opcion salir .</li>
+			</ol>
+			<br>
+			<ul type="none">
+			<li><i class="glyphicon glyphicon-ok"></i> ADMINISTRACION WEB</li>
+			<li><i class="glyphicon glyphicon-ok"></i> PROYECTO FINAL</li>
+			</ul>
+
+	</div>
+	</div>
+	</div>
+	</body>
+</html>
---
+++ php/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost:8889";
+$user="root";
+$password="";
+$db="myapp";
+$con = new mysqli($host,$user,$password,$db);
+
+?>
