d0d3947b91f99c5c14c65d9fad7dd7dcb6a2846a
ry3en <34361537+ry3en@users.noreply.github.com>
2018-10-24 17:14:18 -0500

    links

--- php/navbar.php
+++ php/navbar.php
@@ -2,7 +2,7 @@
   <div class="container"> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar12">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbar12"> <a class="navbar-brand d-none d-md-block" href="#">
+    <div class="collapse navbar-collapse" id="navbar12"> <a class="navbar-brand d-none d-md-block" href="index.php">
         <i class="fa d-inline fa-lg fa-circle"></i>
         <b>MacksJacga</b>
       </a>
@@ -12,8 +12,9 @@
         <li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
       </ul>
       <ul class="navbar-nav">
-        <li class="nav-item"> <a class="nav-link" href="#">Inicia Sesión</a> </li>
-        <li class="nav-item"> <a class="nav-link text-primary" href="#">Regístrate</a> </li>
+        <li class="nav-item"> <a class="nav-link" href="login.php">Inicia Sesión</a> </li>
+        <li class="nav-item"> <a class="nav-link text-primary" href="registro.php">Regístrate</a> </li>
+        <li><a href="./php/logout.php">SALIR</a></li>
       </ul>
     </div>
   </div>
